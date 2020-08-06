c5c832e25678aa02d2d92b251c38d1c00c5fe2ce
Quesmues <eduardoczamanski@protonmail.com>
2020-08-05 23:20:06 -0300

    Tocador de Música

--- main.js
+++ main.js
@@ -1,42 +1,151 @@
 'use strict';
 
-/**
- * A bot that welcomes new guild members when they join
- */
-
-// Import the discord.js module
 const Discord = require('discord.js');
+const ytdl = require('ytdl-core');
 
-// Create an instance of a Discord client
 const client = new Discord.Client();
+
+const queue = new Map();
 
 const {prefix, token} = require('./config.json');
 
-/**
- * The ready event is vital, it means that only _after_ this will your bot start reacting to information
- * received from Discord
- */
 client.on('ready', () => {
   console.log('I am ready!');
 });
 
-client.on('message', message => {
-	if (message.content === `${prefix}massamagra`) {
-        message.channel.send(`T\no\n \nc\no\nm\n \nf\no\nm\ne`);
-    } else if (message.content === `${prefix}server`) {
-        message.channel.send(`Nome do servidor: ${message.guild.name}\nTotal de membros: ${message.guild.memberCount}`);
-    }
+client.on('reconnecting', () => {
+  console.log('Reconnecting!');
 });
 
-// Create an event listener for new guild members
+client.on('disconnect', () => {
+  console.log('Disconnect!');
+});
+
+client.on('message', message => {
+  const serverQueue = queue.get(message.guild.id);
+
+  if (!message.content.startsWith(prefix)) return;
+
+	if (message.content === `${prefix}massamagra`) {
+      message.channel.send(`T\no\n \nc\no\nm\n \nf\no\nm\ne`);
+  } else if (message.content === `${prefix}server`) {
+      message.channel.send(`Nome do servidor: ${message.guild.name}\nTotal de membros: ${message.guild.memberCount}`);
+  } else if (message.content.startsWith(`${prefix}play`)) {
+    execute(message, serverQueue);
+    return;
+  } else if (message.content.startsWith(`${prefix}next`)) {
+    next(message, serverQueue);
+    return;
+  } else if (message.content.startsWith(`${prefix}clear`)) {
+    clear(message, serverQueue);
+    return;
+  }
+});
+
 client.on('guildMemberAdd', member => {
-  // Send the message to a designated channel on a server:
   const channel = member.guild.channels.cache.find(ch => ch.name === 'geral');
-  // Do nothing if the channel wasn't found on this server
   if (!channel) return;
-  // Send the message, mentioning the member
   channel.send(`Bem vindo ${member}, ao servidor dos desocupados na quarentena!!!!!!!!!!!!!`);
 });
 
-// Log our bot in using the token from https://discordapp.com/developers/applications/me
-client.login(process.env.BOT_TOKEN);
+
+async function execute(message, serverQueue) {
+  const args = message.content.split(' ');
+  
+  console.log(args[1])
+  
+  if (typeof args[1] !== 'undefined') {
+    const videoPattern = /^(https?:\/\/)?(www\.)?(m\.)?(youtube\.com|youtu\.?be)\/.+$/gi;
+    const urlValid = videoPattern.test(args[1]);
+
+    if (!urlValid) {
+      return message.channel.send('URL Inválida!');
+    }
+
+    const voiceChannel = message.member.voice.channel;
+    if (!voiceChannel) return message.channel.send('Você precisa estar em um canal de voz para eu poder tocar a música!');
+    const permissions = voiceChannel.permissionsFor(message.client.user);
+    if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
+      return message.channel.send('Preciso de permissões para tocar as músicas!');
+    }
+
+    const songInfo = await ytdl.getInfo(args[1]);
+    const song = {
+      title: songInfo.videoDetails.title,
+      url: songInfo.videoDetails.video_url,
+    };
+
+    if (!serverQueue) {
+      const queueContruct = {
+        textChannel: message.channel,
+        voiceChannel: voiceChannel,
+        connection: null,
+        songs: [],
+        volume: 5,
+        playing: true,
+      };
+
+      queue.set(message.guild.id, queueContruct);
+
+      queueContruct.songs.push(song);
+
+      try {
+        var connection = await voiceChannel.join();
+        queueContruct.connection = connection;
+        play(message.guild, queueContruct.songs[0]);
+      } catch (err) {
+        console.log(err);
+        queue.delete(message.guild.id);
+        return message.channel.send(err);
+      }
+    } else {
+      serverQueue.songs.push(song);
+      console.log(serverQueue.songs);
+      return message.channel.send(`${song.title} foi adicionado a fila!`);
+    }
+  
+  }
+  else {
+    return message.channel.send(`Preciso de uma URL para tocar alguma música!`);
+  }
+
+}
+
+function next(message, serverQueue) {
+	if (!message.member.voiceChannel) return message.channel.send('You have to be in a voice channel to stop the music!');
+	if (!serverQueue) return message.channel.send('There is no song that I could skip!');
+	serverQueue.connection.dispatcher.end();
+}
+
+function clear(message, serverQueue) {
+	if (!message.member.voiceChannel) return message.channel.send('You have to be in a voice channel to stop the music!');
+	serverQueue.songs = [];
+	serverQueue.connection.dispatcher.end();
+}
+
+function play(guild, song) {
+	const serverQueue = queue.get(guild.id);
+
+	if (!song) {
+		serverQueue.voiceChannel.leave();
+		queue.delete(guild.id);
+		return;
+	}
+
+	const dispatcher = serverQueue.connection.play(ytdl(song.url))
+		.on('end', () => {
+			console.log('Music ended!');
+			serverQueue.songs.shift();
+			play(guild, serverQueue.songs[0]);
+		})
+		.on('error', error => {
+			console.error(error);
+		});
+	dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+}
+
+
+client.login('NzQwNzA4OTI4MDA1ODY1NjAz.Xys88g.mi_yho8y9LkTEZLfr8itn_V9QFQ');
+
+
+
